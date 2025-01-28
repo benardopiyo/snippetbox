6fca15f17e3a23f22ed53ebd54f70e41f11e1c00
benardopiyo <benardopiyo13@gmail.com>
2025-01-28 14:59:59 +0300

    feat: create a file server which serves files out of the "./ui/static"directory

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,14 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// create a file server which serves files out of the
+	// "./ui/static"directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server
+	// as the handler for all URL paths that start with "/static"
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	fmt.Println("http://localhost:4000")
 	fmt.Println("Starting Server on :4000")
 	err := http.ListenAndServe(":4000", mux)
