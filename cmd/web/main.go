b9a0b71eef9d251397d5e8410752f672843530f2
benardopiyo <benardopiyo13@gmail.com>
2025-01-28 14:21:16 +0300

    feat: remove the handlers out of main file

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fmt.Println("http://localhost:4000")
+	fmt.Println("Starting Server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
