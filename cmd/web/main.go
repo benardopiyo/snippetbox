4b614e3e6736ffb6692471761c819dc29f5c1eb6
benardopiyo <benardopiyo13@gmail.com>
2025-01-29 13:46:38 +0300

    feat: isolate the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,31 +7,27 @@ import (
 	"os"
 )
 
-func main() {
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
+func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	// create a file server which serves files out of the
-	// "./ui/static"directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server
-	// as the handler for all URL paths that start with "/static"
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 
 	server := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting Server on %s\n", *addr)
